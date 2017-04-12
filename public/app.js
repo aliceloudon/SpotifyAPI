888932c7045d404f77e8edd6887f1f7b7ad56f27
Alice Loudon <alice_loudon@icloud.com>
2017-04-12 22:44:30 +0100

    Build app.js and album_list.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
-var app = function(){
+window.onload = function(){
+  // Set up album list model
+  var albumList = new AlbumList('https://api.spotify.com/v1/search?q=cats&type=album')
+  albumList.getData()
 
+  // Set up views (if any)
 }
-
-window.onload = app;
