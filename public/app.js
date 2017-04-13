22e0611ddd18364fb30fabd4816ae4b0ab1c21ab
Alice Loudon <alice_loudon@icloud.com>
2017-04-13 09:17:50 +0100

    tidy up for submission

--- public/app.js
+++ public/app.js
@@ -1,7 +1,33 @@
-window.onload = function(){
-  // Set up album list model
-  var albumList = new AlbumList('https://api.spotify.com/v1/search?q=cats&type=album')
-  albumList.getData()
-
-  // Set up views (if any)
+var app = function(){
+  var url = 'https://api.spotify.com/v1/search?q=fish&type=album'
+  makeRequest(url, requestComplete)
 }
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
+var requestComplete = function(){
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var albums = JSON.parse(jsonString);
+  populateList(albums);
+}
+
+var populateList = function(albums){
+  var div = document.querySelector('#albums')
+  var ul = document.createElement('ul')
+
+  albums.albums.items.forEach(function(album){
+    var li = document.createElement('li')
+    li.innerText = album.name
+    ul.appendChild('li')      
+    div.appendChild('ul')
+  }) 
+
+}
+
+window.onload = app
